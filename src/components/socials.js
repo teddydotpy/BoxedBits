8dbf0d0720212c69a4af37667c68ff2fc2c8cd8e
Sihle <sihlem.ntombela@gmail.com>
2020-09-18 10:22:49 +0200

    changed font color

--- src/components/socials.js
+++ src/components/socials.js
@@ -5,19 +5,19 @@ const socials = () => (
    <ol> 
         <li className="nav">
             <a href="https://instagram.com/boxedbits"
-            style={{ color: '#d49bd0', textDecoration: 'none'}}>
+            style={{ color: '#ffdab9', textDecoration: 'none'}}>
                 <FeatherIcon icon="instagram" size="30" />
             </a>
         </li>
               <li className="nav">
             <a href="mailto:boxedbitsnthings@gmail.com"
-            style={{ color: '#d49bd0', textDecoration: 'none'}}>
+            style={{ color: '#ffdab9', textDecoration: 'none'}}>
                 <FeatherIcon icon="mail" size="30" />
             </a>
         </li>
               <li className="nav">
             <a href="tel:0683686451"
-            style={{ color: '#d49bd0', textDecoration: 'none'}}>
+            style={{ color: '#ffdab9', textDecoration: 'none'}}>
                 <FeatherIcon icon="phone" size="30" />
             </a>
         </li>
